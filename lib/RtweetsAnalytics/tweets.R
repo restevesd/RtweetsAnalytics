7ddacf0a67574f0849901a21861d1c6bb670b1eb
Bartek Skorulski <bartekskorulski@gmail.com>
2015-06-12 13:23:56 +0200

    adds basic statystics

--- lib/RtweetsAnalytics/tweets.R
+++ lib/RtweetsAnalytics/tweets.R
@@ -1,10 +1,28 @@
 require(ggplot2)
 
-createdPlot <- function(tweets.df) {
-  new.ts <- data.frame(dt=as.POSIXct(tweets.df$created, origin = "1970-01-01"))
-  new.ts$day <- as.Date(new.ts$dt)
-  
-  ggplot(data=new.ts) + geom_freqpoly(aes(dt), binwidth=10000)+
-    theme_bw() + xlab('DateTime')
-  
+noRetwitted <- function(tweets.df) {
+  subset(tweets.df, isRetweet==0)
 }
+
+retwitted <- function(tweets.df) {
+  subset(tweets.df, isRetweet==1)
+}
+
+toDateTime <- function(n) {
+  as.POSIXct(n, origin = "1970-01-01")
+}
+
+
+freqPlotAll <- function(tweets.df) {
+  ggplot(data=tweets.df) + geom_freqpoly(aes(toDateTime(created)), binwidth=10000)+
+    theme_bw() + xlab('DateTime')  
+}
+
+freqPlotByTRT <- function(tweets.df) {
+  ggplot(data=tweets.df) +
+    geom_freqpoly(aes(toDateTime(created), colour=as.factor(isRetweet)), binwidth=10000) +
+    theme_bw() + xlab('DateTime')  
+}
+
+
+

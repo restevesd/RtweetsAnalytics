38a48652afdcdf5da539d0c6d77996adaddf5d99
Bartek Skorulski <bartekskorulski@gmail.com>
2015-06-13 16:51:33 +0200

    adds dates restriction

--- examples/basicStatistics.R
+++ examples/basicStatistics.R
@@ -8,6 +8,8 @@ source('RtweetsAnalytics.R')
 hash.txt <- getAllHashes()[1,1]
 tweets.df <- getTweetsFromDB(hash.txt, n.tweets=100)
 
+head(tweets.df$created)
+
 print(paste('Total number of tweets:', dim(tweets.df)[1]))
 print(paste('Number of retweets:', dim(retwitted(tweets.df))[1]))
 print(paste('Number of no retwitted tweets:', dim(noRetwitted(tweets.df))[1]))
@@ -15,4 +17,11 @@ print(paste('Number of no retwitted tweets:', dim(noRetwitted(tweets.df))[1]))
 freqPlotAll(tweets.df)
 freqPlotByTRT(tweets.df)
 
+tweets.df$created > 1
+
+freqPlotByTRT(tweets.df, "2015-06-04") 
+freqPlotByTRT(tweets.df, end.date="2015-06-09")
+freqPlotByTRT(tweets.df, "2015-06-04", "2015-06-09")
+freqPlotByTRT(tweets.df, "2015-06-04", "2015-06-20") +
+  scale_colour_discrete(labels=c("Control", "Treatment 1"))
 
